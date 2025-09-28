c969fbdc8760ae375e204839449bcf405e173e64
unknown <kepera@inbox.ru>
2025-09-28 21:34:52 +0300

    Admin panel

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-frontend-domain.com'] 
+    ? ['https://pro-investor-front.vercel.app'] 
     : ['http://localhost:3000'],
   credentials: true
 }));
